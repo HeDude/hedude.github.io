0795611b1d875b25892e898e76581c65dab5ec49
slagter@bijbrengen.nl <slagter@bijbrengen.nl>
2023-10-20 08:28:10 +0200

    Added navigation functionality to only show the description which is choosen

---
+++ js/script.js
@@ -0,0 +1,29 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    // Get all the navigation panels and descriptions
+    const navPanels = document.querySelectorAll('.panel-keep-ratio');
+    const descriptions = document.querySelectorAll('.description');
+
+    // Initially hide all descriptions
+    descriptions.forEach(function (description) {
+        description.style.display = 'none';
+    });
+
+    // Add click event listener to each navigation panel
+    navPanels.forEach(function (panel) {
+        panel.addEventListener('click', function () {
+            // Hide all descriptions
+            descriptions.forEach(function (description) {
+                description.style.display = 'none';
+            });
+
+            // Get the ID of the clicked panel
+            const panelId = panel.getAttribute('data-nav-item-id');
+
+            // Show the corresponding description
+            const description = document.querySelector(`[data-page-id="${panelId}"]`);
+            if (description) {
+                description.style.display = 'block';
+            }
+        });
+    });
+});
