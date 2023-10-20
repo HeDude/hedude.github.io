0a81dd07fb66f4f9ee6c7ce11fdd49143d6bdc33
slagter@bijbrengen.nl <slagter@bijbrengen.nl>
2023-10-20 09:02:42 +0200

    Added flipping background with navigation choice

--- js/script.js
+++ js/script.js
@@ -18,12 +18,20 @@
 
             // Get the ID of the clicked panel
             const panelId = panel.getAttribute('data-nav-item-id');
+            console.log("Clicked Panel ID:", panelId);
+
 
             // Show the corresponding description
             const description = document.querySelector(`[data-page-id="${panelId}"]`);
             if (description) {
                 description.style.display = 'block';
             }
+
+            // Change the background image based on the clicked panel
+            const bgName = panelId.split('-')[1]; 
+            const bgFileName = `${bgName}-bg.png`; 
+            document.body.style.backgroundImage = `url('image/${bgFileName}')`;
         });
     });
+
 });
